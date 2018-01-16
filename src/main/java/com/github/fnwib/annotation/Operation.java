9f2815ec30339437ea7c46d80214e5b251aa860d
zzwb <meetfenng@gmail.com>
2018-01-16 18:08:53 +0800

    remove

--- src/main/java/com/github/fnwib/annotation/Operation.java
+++ src/main/java/com/github/fnwib/annotation/Operation.java
@@ -1,10 +1,5 @@
 package com.github.fnwib.annotation;
 
-/**
- * value == 0  不应该匹配到任务Excel列
- * value == 1  只能匹配到最多一列
- * value == 2  可以匹配到任意数量列
- */
 public enum Operation {
     LINE_NUM,
     DEFAULT,
